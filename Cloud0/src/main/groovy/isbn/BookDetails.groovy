b7d2917c9c8bbee62e129324c021fda98cc4f41b
txt2vz <lauriehirsch@gmail.com>
2018-01-18 12:44:44 +0000

    first

---
+++ Cloud0/src/main/groovy/isbn/BookDetails.groovy
@@ -0,0 +1,29 @@
+package isbn
+
+import javax.servlet.ServletConfig
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import groovy.json.JsonSlurper
+import groovy.servlet.GroovyServlet
+
+
+class BookDetails extends GroovyServlet{
+
+	void init(ServletConfig config) {
+		System.out.println " UrlToJSON Servlet initialized"
+	}
+
+	void service(HttpServletRequest request, HttpServletResponse response) {
+		
+		String strISBN = request.getParameter("isbn");
+		String googleBooksURL = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+strISBN 
+		def url = googleBooksURL.toURL()
+		def json = url.getText()
+		println "xx " + json
+		def jsonSlurper = new JsonSlurper()
+		def object = jsonSlurper.parseText(json)
+		def title = object.items[0].volumeInfo.title	
+
+		response.getWriter().println(title)
+	}
+}
